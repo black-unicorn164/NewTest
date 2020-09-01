23920df7808c910986cae815ac849a3bb8b37339
Shvetsova Olga <black-unicorn@ya.ru>
2020-09-01 11:32:26 +0300

    Initial Commit

---
+++ NewTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewTest
+//
+//  Created by Давлетбаев АВ on 01.09.2020.
+//  Copyright © 2020 Shvetsova Olga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
