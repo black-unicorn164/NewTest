077737ad91226a82dd18a15f5a97f6c00bec5150
Shvetsova Olga <black-unicorn@ya.ru>
2020-09-03 12:32:28 +0300

    Работа с интерфейсом

--- NewTest/ViewController.swift
+++ NewTest/ViewController.swift
@@ -1,20 +1,25 @@
-//
-//  ViewController.swift
-//  NewTest
-//
-//  Created by Давлетбаев АВ on 01.09.2020.
-//  Copyright © 2020 Shvetsova Olga. All rights reserved.
-//
+    //
+    //  ViewController.swift
+    //  NewTest
+    //
+    //  Created by Давлетбаев АВ on 01.09.2020.
+    //  Copyright © 2020 Shvetsova Olga. All rights reserved.
+    //
 
-import UIKit
+    import UIKit
 
-class ViewController: UIViewController {
+        class ViewController: UIViewController {
+         
+            @IBOutlet var helloWorldLabel: UILabel!
+            @IBOutlet var showTextButton: UIButton!
+            
+            override func viewDidLoad() {
+                super.viewDidLoad()
+                // Do any additional setup after loading the view, typically from a nib.
+            }
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-
-}
+            @IBAction func showTextButtonPressed() {
+            }
+            
+        }
 
